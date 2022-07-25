b65eb343961373013fe03dd17b2935668d479ba7
wis-ngops <109586766+wis-ngops@users.noreply.github.com>
2022-07-25 16:16:30 +0700

    Add files via upload

---
+++ prototype/styling2.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+  margin:5,
+  loop:true,
+  autoWidth:true,
+  nav:true,
+  autoplay:true,
+  responsiveClass:true,
+  autoplayTimeout:4000,
+})
+
